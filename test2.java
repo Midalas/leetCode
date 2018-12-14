82f97db31cc5579f45c6f1c19b342c172cf54312
Midalas <672295473@qq.com>
2018-12-14 18:16:30 +0800

    20181214

--- test2.java
+++ test2.java
@@ -1,36 +1,40 @@
 package leetCode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class test2 {
 	public static void main(String[] args) throws Exception {
 
-		int target = 18;
-		int x = reachNumber(target);
+		TreeNode root = leetCode297.deserialize("0,#,1,2,5,#,3,#,#,#,4");
+
+		List<Integer> x = distanceK(root, root.right.left, 2);
 		System.out.println();
 	}
 
-	
-	//runtime 9ms
-	public static int reachNumber(int target) {
-		if (target < 0)
-			target = -target;
-		int sum = 0;
-		int i = 1;
-		while (sum < target) {
-			sum += i++;
-		}
-		if (sum == target) {
-			return i - 1;
-		}
-		if ((sum - target) % 2 == 0) {
-			return i - 1;
-		} else {
-			if ((sum + i - target) % 2 == 0) {
-				return i;
-			} else {
-				return i + 1;
+	private static int[] arr = new int[501];
+	public static List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
+		List<Integer> res = new ArrayList<Integer>();
+		Arrays.fill(arr, 1023);
+		arr[root.val] = 0;
+		dfs(root.left, -1, -1);
+		dfs(root.right, 1, 1);
+		int aim = arr[target.val];
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] < 1023 && Math.abs(arr[i] - aim) == K) {
+				res.add(i);
 			}
-
 		}
+		return res;
+	}
+
+	private static void dfs(TreeNode root, int level, int offset) {
+		if (root == null)
+			return;
+		arr[root.val] = level;
+		dfs(root.left, level + offset, offset);
+		dfs(root.right, level + offset, offset);
 	}
 
 }
